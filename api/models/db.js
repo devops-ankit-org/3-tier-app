1ff28b0bafc3746bb54fada41fb33f652d896e4d
Ankit Sarkar <92983704+ankitsarkar31@users.noreply.github.com>
2025-07-07 21:04:48 +0530

    Update db.js

--- api/models/db.js
+++ api/models/db.js
@@ -1,42 +1,19 @@
-// require('dotenv').config();
-// const mysql = require('mysql2');
-
-// const db = mysql
-//   .createPool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0,
-//     ssl: {
-//       rejectUnauthorized: false
-//     }
-//   });
-
-// module.exports = db;
+require('dotenv').config();
 const mysql = require('mysql2');
 
-// Create a connection to the database
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST, // Cloud SQL instance IP or connection name
-  user: process.env.DB_USER, // Database username
-  password: process.env.DB_PASSWORD, // Database password
-  database: process.env.DB_NAME, // Database name
-  // Additional options for Cloud SQL
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const db = mysql
+  .createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
 
-// Connect to the database
-connection.connect(error => {
-  if (error) {
-    console.error('Error connecting to the database:', error);
-    return;
-  }
-  console.log('Connected to the database.');
-});
+module.exports = db;
 
-module.exports = connection;
